ce32c89f63a2fb010741f4594976e0c3d772b88e
dimsrzqy <dimasrizqyilhami@gmal.com>
2025-05-31 11:23:55 +0700

    Merge origin/master into dimas
    # Conflicts:
    #	src/Panel/PanelManajemenBarang.form
    #	src/Panel/PanelManajemenBarang.java

---
+++ src/Panel/PanelPenjualanDTL.java
@@ -0,0 +1,28 @@
+package Panel;
+
+public class PanelPenjualanDTL extends javax.swing.JPanel {
+
+    public PanelPenjualanDTL() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 684, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 489, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
